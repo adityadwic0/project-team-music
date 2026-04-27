cdcafce139661c175642b4529e8b82bb82390f51
Destrian <destrianwww@gmail.com>
2026-04-27 14:17:44 +0700

    tes doang

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HeavyController;
 use App\Http\Controllers\StressTestController;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
